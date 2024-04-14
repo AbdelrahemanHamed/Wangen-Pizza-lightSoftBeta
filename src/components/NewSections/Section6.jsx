e67299b8ae49dd5726b60f62723b0c0d98c08d06
Abdelraheman Hamed <abdelrahemanhamed@gmail.com>
2024-04-14 21:48:33 +0200

    first commit

---
+++ src/components/NewSections/Section6.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./section6.css";
+const Section6 = () => {
+  return (
+    <div className="section6">
+      <div className="content-section6">
+        <h1>Wangen Pizza und Kebab Kurier
+         Lieferzonen und Mindestbestellbetrag</h1>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>
+        Zone-1: Wangen, Nuolen, Lachen --------------------------------------------------------25.<br/>
+        Zone-2: Buttikon, Schübelbach, Galgenen, Tuggen, Siebnen-------------------30.<br/>
+        Zone-3: Reichenburg, Altendorf-----------------------------------------------------------40.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Section6;
